600e537cda8328bc53106e618a0b71450c80fa26
GalvinPython <77013913+GalvinPython@users.noreply.github.com>
2024-04-01 20:31:18 +0100

    #29: added odometer

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import {
-    HighchartsChart,
-    Chart,
-    XAxis,
-    YAxis,
-    Title,
-    LineSeries,
-    Tooltip,
-    HighchartsProvider,
-} from 'react-jsx-highcharts';
+import { HighchartsChart, Chart, XAxis, YAxis, Title, LineSeries, Tooltip, HighchartsProvider } from 'react-jsx-highcharts';
 import Highcharts from 'highcharts';
+import OdometerComponent from './Odometer'; // Import the Odometer component
 
 interface DataPoint {
     x: number;
@@ -23,6 +15,7 @@ interface CustomChartProps {
 
 const CustomChart = ({ fetchClicks, clientData }: CustomChartProps) => {
     const [data, setData] = useState<DataPoint[]>([]);
+    const [odometerValue, setOdometerValue] = useState(0); // State to hold the odometer value
 
     useEffect(() => {
         const fetchData = async () => {
@@ -39,6 +32,9 @@ const CustomChart = ({ fetchClicks, clientData }: CustomChartProps) => {
                 // 60 minutes = 3600 seconds, however 1 request every 2 seconds = 1800 datapoints for an hour
                 // Minus 1 for 1799 because I don't know, JavaScript moment
                 setData(prevData => [...prevData.slice(-1799), newDataPoint]);
+
+                // Update the odometer value
+                setOdometerValue(jsonData[0].row_count);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -50,47 +46,59 @@ const CustomChart = ({ fetchClicks, clientData }: CustomChartProps) => {
             return () => clearInterval(interval);
         } else if (clientData) {
             setData(clientData);
+            // If client data is provided, set the odometer value
+            if (clientData.length > 0) {
+                setOdometerValue(clientData[clientData.length - 1].y);
+            }
         }
     }, [fetchClicks, clientData]);
 
     return (
-        <HighchartsProvider Highcharts={Highcharts}>
-            <HighchartsChart plotOptions={{}}>
-                <Chart backgroundColor={'transparent'} />
+        <div>
+            {/* Render the Odometer component with the current odometer value */}
+            <OdometerComponent value={odometerValue} />
 
-                <Title style={{ color: '#fff' }}>Clicks</Title>
-                <Tooltip
-                    padding={10}
-                    hideDelay={250}
-                    shape='rect'
-                    split
-                    backgroundColor={'black'}
-                    style={{ color: 'white' }}
-                />
-                <XAxis
-                    tickColor={'#fff'}
-                    gridLineColor={'#fff'}
-                    minorGridLineColor={'#fff'}
-                    lineColor={'#fff'}
-                    labels={{ style: { color: '#fff' } }}
-                    type='datetime'>
-                </XAxis>
-                <YAxis
-                    id='number'
-                    tickColor={'#fff'}
-                    gridLineColor={'#fff'}
-                    minorGridLineColor={'#fff'}
-                    lineColor={'#fff'}
-                    labels={{ style: { color: '#fff' } }}>
-                    
-                    <LineSeries
-                        name='Row Count'
-                        id='clicksChart'
-                        data={data}
+            {/* Render the Highcharts chart */}
+            <HighchartsProvider Highcharts={Highcharts}>
+                <HighchartsChart plotOptions={{}}>
+                    <Chart backgroundColor={'transparent'} />
+
+                    <Title style={{ color: '#fff' }}>Clicks</Title>
+                    <Tooltip
+                        padding={10}
+                        hideDelay={250}
+                        shape='rect'
+                        split
+                        backgroundColor={'black'}
+                        style={{ color: 'white' }}
                     />
-                </YAxis>
-            </HighchartsChart>
-        </HighchartsProvider>
+                    <XAxis
+                        tickColor={'#fff'}
+                        gridLineColor={'#fff'}
+                        minorGridLineColor={'#fff'}
+                        lineColor={'#fff'}
+                        labels={{ style: { color: '#fff' } }}
+                        type='datetime'>
+                    </XAxis>
+                    <YAxis
+                        id='number'
+                        tickColor={'#fff'}
+                        gridLineColor={'#fff'}
+                        minorGridLineColor={'#fff'}
+                        lineColor={'#fff'}
+                        labels={{ style: { color: '#fff' } }}>
+
+                        <LineSeries
+                            name='Row Count'
+                            id='clicksChart'
+                            data={data}
+                            color={'rgb(136, 58, 234)'}
+                            lineWidth={5}
+                        />
+                    </YAxis>
+                </HighchartsChart>
+            </HighchartsProvider>
+        </div>
     );
 };
 
---
+++ src/components/Odometer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Odometer from 'react-odometerjs';
+import './Odometer.css'
+
+const OdometerComponent = ({ value }: { value: number }) => {
+    return <Odometer className='odometer' value={value} format="(,ddd)" />;
+};
+
+export default OdometerComponent;
